cd16bee06ed58df71ce2531226a3d8c72af0988d
jithinTB <jithintb805@gmail.com>
2025-08-26 04:53:40 -0700

    about page responsive

--- js/script.js
+++ js/script.js
@@ -458,20 +458,21 @@ function showAndHide(passwordField, loginHidePasswordIcon, loginShowPasswordIcon
 
 	// Testimonial slider
 
- var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-    //   spaceBetween: 30,
-	grabCursor:true,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+//  var swiper = new Swiper(".mySwiper", {
+	
+//       slidesPerView: 1,
+//     //   spaceBetween: 30,
+// 	grabCursor:true,
+//       loop: true,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     });
 
 
 
